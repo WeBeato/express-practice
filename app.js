297abba8428f7beb988a9b4e1e251aa545aaaf4c
WeBeato <webeato.ad@gmail.com>
2025-10-20 23:54:55 +0330

    update

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ mongoose.connect(dbURI)
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
 
+
 // Static Files
 app.use(express.static('public'))
 
@@ -31,45 +32,74 @@ app.use(express.static('public'))
 // })
 app.use(morgan('tiny'));
 
-// adding blog
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog 2',
-        body: 'Lorem ipsum dolor sit amet consectetur.'
-    });
 
-    blog.save()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-})
+// // adding blog
+// app.get('/add-blog', (req, res) => {
+//     const blog = new Blog({
+//         title: 'new blog 2',
+//         snippet: 'about my new blog 2',
+//         body: 'Lorem ipsum dolor sit amet consectetur.'
+//     });
 
-// getting all blogs
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-})
-
-// getting single blog
-app.get('/single-blog', (req, res) => {
-    Blog.findById('68f279735fb17547b89b8a1e')
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-})
+//     blog.save()
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// })
 
 
+// // getting all blogs
+// app.get('/all-blogs', (req, res) => {
+//     Blog.find()
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// })
+
+// // getting single blog
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('68f279735fb17547b89b8a1e')
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// })
+
+
+// // deleting single blog
+// app.get('/d-single-blog', (req, res) => {
+//     Blog.findByIdAndDelete('68f694683c9aa278efcd5b3a')
+//         .then((result) => {
+//             if (result) {
+//                 res.send(`Document Deleted: \n ${result}`)
+//             } else {
+//                 res.send(`Document not found!!`)
+//             }
+//         })
+//         .catch((err) => console.log(err));
+// })
+
+
+// // deleting all blog
+// app.get('/d-all-blogs', (req, res) => {
+//     Blog.deleteMany({})
+//         .then((result) => res.send(result))
+//         .catch((err) => console.log(err));
+// })
+
+
+// Routes
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Bahram in Journey', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
-        { title: 'How to Fix Bugs', snippet: 'Lorem ipsum dolor sit amet consectetur.' },
-        { title: 'NASA Finds Water in a Planet', snippet: 'Lorem ipsum dolor sit amet consectetur.' }
-    ]
-    res.render('index', { title: 'Home', blogs })
+    res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' })
+})
+
+// Blog Routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((blogs) => {
+            res.render('index', { title: 'All Blogs', blogs })
+        })
+        .catch((err) => console.log(err))
 })
 
 app.get('/blogs/create', (req, res) => {
