11c93ae6167bbd7024c2fbfe83734dbaccd4e30a
WeBeato <webeato.ad@gmail.com>
2025-10-21 15:02:57 +0330

    update

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ mongoose.connect(dbURI)
     .catch((err) => console.log(err));
 
 
-// Static Files
+// Middleware & Static Files
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
 
 // app.use((req, res,next) => {
 //     console.log('New Request was Made:');
@@ -95,12 +96,44 @@ app.get('/about', (req, res) => {
 
 // Blog Routes
 app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt: -1})
+    Blog.find().sort({ createdAt: -1 })
         .then((blogs) => {
             res.render('index', { title: 'All Blogs', blogs })
         })
         .catch((err) => console.log(err))
 })
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => console.log(err))
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => console.log(err))
+})
+
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch(err => console.log(err))
+})
+
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a New Blog' })
